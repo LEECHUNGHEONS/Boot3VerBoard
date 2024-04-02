4aa5dc7906fb572cd2da082c0f602c70880526b2
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-04-02 13:09:57 +0900

    Add Class AddArticleRequest

---
+++ src/main/java/ch/blog/boot3ver_blog/dto/AddArticleRequest.java
@@ -0,0 +1,12 @@
+package ch.blog.boot3ver_blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+
+}
