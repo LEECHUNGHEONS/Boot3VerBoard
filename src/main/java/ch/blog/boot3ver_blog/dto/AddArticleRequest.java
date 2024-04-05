eedf5bf134a0925077b417ca84fd7ee1981aac44
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-04-05 13:01:39 +0900

    Add Class AddArticleRequest - toEntity Method

--- src/main/java/ch/blog/boot3ver_blog/dto/AddArticleRequest.java
+++ src/main/java/ch/blog/boot3ver_blog/dto/AddArticleRequest.java
@@ -1,5 +1,6 @@
 package ch.blog.boot3ver_blog.dto;
 
+import ch.blog.boot3ver_blog.domain.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,11 @@ public class AddArticleRequest {
     private String title;
     private String content;
 
+
+    public Article toEntity(){
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
 }
