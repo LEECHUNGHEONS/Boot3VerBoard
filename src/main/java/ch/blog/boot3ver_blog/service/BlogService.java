c19339c8533684e2fab556c3fb52cfe83c46806a
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-04-07 13:08:05 +0900

    Add Class BlogService

---
+++ src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
@@ -0,0 +1,10 @@
+package ch.blog.boot3ver_blog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service // bean으로 등록
+public class BlogService {
+
+}
