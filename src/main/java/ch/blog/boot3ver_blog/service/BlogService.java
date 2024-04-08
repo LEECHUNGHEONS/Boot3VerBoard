97ccc64fd7cc42f6f3138729da3ffc55e04fb6bf
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-04-08 13:08:05 +0900

    Add BlogService field

--- src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
+++ src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
@@ -1,5 +1,6 @@
 package ch.blog.boot3ver_blog.service;
 
+import ch.blog.boot3ver_blog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Service;
 @Service // bean으로 등록
 public class BlogService {
 
+    private final BlogRepository blogRepository;
 }
