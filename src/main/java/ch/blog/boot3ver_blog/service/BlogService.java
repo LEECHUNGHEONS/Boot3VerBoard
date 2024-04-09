01027f2c5b77335b134898f024b44e0a0dd691a0
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-04-09 13:58:10 +0900

    Add BlogService Save Method

--- src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
+++ src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
@@ -1,5 +1,7 @@
 package ch.blog.boot3ver_blog.service;
 
+import ch.blog.boot3ver_blog.domain.Article;
+import ch.blog.boot3ver_blog.dto.AddArticleRequest;
 import ch.blog.boot3ver_blog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Service;
 public class BlogService {
 
     private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
 }
