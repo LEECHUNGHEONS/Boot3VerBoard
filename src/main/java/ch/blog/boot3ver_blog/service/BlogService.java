7b6ae87e6291cf1a50befefa5d635a811ece8c04
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-04-13 13:11:07 +0900

    Add Exp

--- src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
+++ src/main/java/ch/blog/boot3ver_blog/service/BlogService.java
@@ -14,10 +14,12 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
+    //블로그 (게시판) 글 등록 메서드
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
     }
 
+    // 모든 게시글을 가져오는 서비스 로직
     public List<Article> findAll() { return blogRepository.findAll();}
 
     public Article findbyId(Long id){
